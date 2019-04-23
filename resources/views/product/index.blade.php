ffae41a04c35fa2e7c5757e722193bfa2e59bb8d
Djurre <djurre10@hotmail.com>
2019-04-23 13:08:48 +0200

    Sepperation

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -20,34 +20,7 @@
 
         <div class="row">
             <div class="col-md-9">
-                @foreach($products as $product)
-                    <div class="card mb-3">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                <img src="..." class="card-img" alt="...">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ $product->name }}</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural
-                                        lead-in to additional content. This content is a little bit longer.</p>
-                                    <p class="card-text">{{ $product->price }}</p>
-                                    <p class="card-text">{{ $product->category_id }}</p>
-                                    <form action="{{ route('product.destroy', $product->id) }}" method="post">
-                                        <a class="btn btn-sm btn-success"
-                                           href="{{ route('product.show', $product->id) }}">Show</a>
-                                        <a class="btn btn-sm btn-warning"
-                                           href="{{ route('product.edit', $product->id) }}">Edit</a>
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-sm btn-danger">Delete</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-                {!! $products->links() !!}
+                @include('includes.product')
             </div>
             <div class="col-md-3">
                 <ul>
